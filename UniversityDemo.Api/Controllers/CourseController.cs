c5b9d785c381d82b4e3a3b2e0aa113c13a418fc3
hari98sv <harisankarv98@gmail.com>
2023-03-25 19:20:20 +0530

    Added Api project and CourseController

---
+++ UniversityDemo.Api/Controllers/CourseController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UniversityDemo.Api.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("[controller]")]
+    public class CourseController : ControllerBase
+    {
+       
+    }
+}
