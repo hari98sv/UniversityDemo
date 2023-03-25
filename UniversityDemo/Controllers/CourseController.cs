8e26fd87f0c6cd079b9635046c66f7dc30f1a377
hari98sv <harisankarv98@gmail.com>
2023-03-25 12:39:19 +0530

    Added autherization for Course

--- UniversityDemo/Controllers/CourseController.cs
+++ UniversityDemo/Controllers/CourseController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,6 +9,7 @@ using UniversityDemo.Application.ViewModels;
 
 namespace UniversityDemo.Controllers
 {
+    [Authorize]
     public class CourseController : Controller
     {
         private ICourseService _courseService;
