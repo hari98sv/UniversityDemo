4bf903871e446427a0e56143dcfedaaeb6e6c478
hari98sv <harisankarv98@gmail.com>
2023-03-25 19:08:08 +0530

    Added Create course functionality in Application layer

--- UniversityDemo.Application/Interfaces/ICourseService.cs
+++ UniversityDemo.Application/Interfaces/ICourseService.cs
@@ -10,5 +10,7 @@ namespace UniversityDemo.Application.Interfaces
     public interface ICourseService
     {
         CourseViewModel GetCourses();
+
+        void Create(CourseViewModel courseViewModel);
     }
 }
--- UniversityDemo.Application/Services/CourseService.cs
+++ UniversityDemo.Application/Services/CourseService.cs
@@ -5,17 +5,31 @@ using System.Text;
 using System.Threading.Tasks;
 using UniversityDemo.Application.Interfaces;
 using UniversityDemo.Application.ViewModels;
+using UniversityDemo.Domain.Commands;
+using UniversityDemo.Domain.Core.Bus;
 using UniversityDemo.Domain.Interfaces;
 
 namespace UniversityDemo.Application.Services
 {
     public class CourseService : ICourseService
     {
-        private ICourseRepository _courseRepository;
+        private readonly ICourseRepository _courseRepository;
+        private readonly IMediatorHandler _bus;
 
-        public CourseService(ICourseRepository courseRepository)
+        public CourseService(ICourseRepository courseRepository, IMediatorHandler bus)
         {
             _courseRepository = courseRepository;
+            _bus = bus;
+        }
+
+        public void Create(CourseViewModel courseViewModel)
+        {
+            var createCourseCommand = new CreateCourseCommand(
+                                            courseViewModel.Name,
+                                            courseViewModel.Description,
+                                            courseViewModel.ImageUri);
+
+            _bus.SendCommand(createCourseCommand);
         }
 
         public CourseViewModel GetCourses()
