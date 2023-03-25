23a97b0ea6b5bd73c40e5a1ee4df337abf3cbd90
hari98sv <harisankarv98@gmail.com>
2023-03-25 13:16:33 +0530

    Added Command and handlers for Courses

---
+++ UniversityDemo.Domain/CommandHandlers/CourseCommandHandler.cs
@@ -0,0 +1,38 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using UniversityDemo.Domain.Commands;
+using UniversityDemo.Domain.Interfaces;
+using UniversityDemo.Domain.Models;
+
+namespace UniversityDemo.Domain.CommandHandlers
+{
+    public class CourseCommandHandler : IRequestHandler<CourseCommand, bool>
+    {
+        private readonly ICourseRepository _courseRepository;
+
+        public CourseCommandHandler(ICourseRepository courseRepository)
+        {
+            _courseRepository = courseRepository;
+        }
+
+        public Task<bool> Handle(CourseCommand request, CancellationToken cancellationToken)
+        {
+            var course = new Course()
+            {
+                Name = request.Name,
+                Description = request.Description,
+                ImageUri = request.ImageUri
+            };
+
+            _courseRepository.Add(course);
+
+            return Task.FromResult(true);
+
+        }
+    }
+}
---
+++ UniversityDemo.Domain/Commands/CreateCourseCommand.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniversityDemo.Domain.Commands
+{
+    public class CreateCourseCommand : CourseCommand
+    {
+        public CreateCourseCommand(string name, string desciption, string imageUri)
+        {
+            Name = name;
+            Description = desciption;
+            ImageUri = imageUri;
+        }
+    }
+}
