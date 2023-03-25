20340880882e16524f494caaea0983bd5aa44575
hari98sv <harisankarv98@gmail.com>
2023-03-25 19:25:12 +0530

    Updated Api configuration

--- UniversityDemo.Api/Startup.cs
+++ UniversityDemo.Api/Startup.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -11,6 +12,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using UniversityDemo.Infra.Data.Context;
+using UniversityDemo.Infra.IoC;
 
 namespace UniversityDemo.Api
 {
@@ -32,6 +35,13 @@ namespace UniversityDemo.Api
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "UniversityDemo.Api", Version = "v1" });
             });
+
+            services.AddDbContext<UniversityDBContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("UniversityDBConnection"));
+            });
+
+            RegisterServices(services);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -54,6 +64,11 @@ namespace UniversityDemo.Api
             {
                 endpoints.MapControllers();
             });
+        }
+
+        private static void RegisterServices(IServiceCollection services)
+        {
+            DependencyContainer.RegisterServices(services);
         }
     }
 }
